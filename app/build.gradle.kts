b1d956048c9827b871f3ccd176fe46ee545a44f2
austineyoung2000 <austineyoung2000@gmail.com>
2026-04-25 02:18:45 -0400

    Bump app version to 1.0.2rc1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.elitedarkkaiser.redmagic"
         minSdk = 28
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.1rc2"
+        versionCode = 4
+        versionName = "1.0.2rc1"
     }
 
     signingConfigs {
